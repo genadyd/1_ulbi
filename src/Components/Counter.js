32566b48d50319b3ec10fed89de6302166b7bca5
genady <jobgenady@gmail.com>
2022-04-06 22:42:20 +0300

    funcComponent

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,12 +1,25 @@
 import React, {useState} from 'react';
+import {Button} from "react-bootstrap";
 
 const Counter = () => {
     const [counter, setCounter] = useState(0)
+    const counterPlus = () => {
+        let currentCounter = counter
+        currentCounter++
+        setCounter(currentCounter)
+    }
+    const counterMinus = () => {
+        let currentCounter = counter
+        currentCounter--
+        setCounter(currentCounter)
+    }
+
     return (
-        <div>
-            counter
-        </div>
-    )
+        <div className="container pt-1">
+            <Button className="me-2" onClick={counterPlus}>Increment</Button>
+            <Button onClick={counterMinus}>Decrement</Button>
+            <div className="res">{counter}</div>
+        </div>)
 }
 
 export default Counter;
