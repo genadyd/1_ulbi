b7d3d421eca6b7eaa3ed1d4c331a0d97a654728d
genady <jobgenady@gmail.com>
2022-04-07 03:47:48 +0300

    create form

---
+++ src/Components/FormsComponents/AddPost.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Button, Form} from "react-bootstrap";
+
+const AddPost = (props) => {
+
+    const {posts, setPostList} = props
+    console.log(posts)
+    return (
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicTitle">
+                <Form.Label>Post title</Form.Label>
+                <Form.Control type="text" placeholder="Enter post title" />
+                <Form.Text className="text-muted">
+                    Type the new post title here
+                </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicText">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="text" placeholder="Post text" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Add Post
+            </Button>
+        </Form>
+    );
+};
+
+export default AddPost;
--- src/Components/PostsList/PostsList.js
+++ src/Components/PostsList/PostsList.js
@@ -1,26 +1,29 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import Post from "./Post";
-import {getPosts} from "../Api/Posts";
+import {getPosts} from "../../Api/Posts";
+import AddPost from "../FormsComponents/AddPost";
 
 
 const PostsList = () => {
-    const[posts, setPostsList] = useState([])
+    const [posts, setPostsList] = useState([])
 
 
-    useEffect(()=>{
+    useEffect(() => {
         getPosts().then(responce => {
-            let resPosts = responce.data.filter((item, index)  => index < 20)
+            let resPosts = responce.data.filter((item, index) => index < 20)
             setPostsList(resPosts)
         })
-            .catch (e =>console.error(`Error: ${e}`))
-    },[])
+            .catch(e => console.error(`Error: ${e}`))
+    }, [])
 
     return (
-        <div className="container border pb-2">
-            <h3>Posts List</h3>
-            {posts.map((post => <Post key={post.id} {...post}/> ))}
-        </div>
+            <div className="container border pb-2">
+                <AddPost posts={posts} setPostList={setPostsList}/>
+                <hr/>
+                <h3>Posts List</h3>
+                {posts.map((post => <Post key={post.id} {...post}/>))}
+            </div>
     )
 }
 
